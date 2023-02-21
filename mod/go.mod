79607db13ad95500aebdd73f988ce90a78697ce7
your name <you@example.com>
2023-02-21 15:45:28 +0800

    mod

--- mod/go.mod
+++ mod/go.mod
@@ -1,3 +1,13 @@
 module example.com/m
 
 go 1.19
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	gorm.io/gorm v1.24.5 // indirect
+)
--- mod/main.go
+++ mod/main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 
 	"example.com/m/calc"
+	"github.com/shopspring/decimal"
 )
 
 // 1、 go mod init example.com/m
-// 2、
+// 2、 新建文件夹即可创建包，通过引入example.com/m/calc，就可以用首字母大写来调用
+// 3、 第三方包 go get github.com/shopspring/decimal
+// 4、 go vendor 表示把mod 放到vendor文件夹，优先从这里查找模块
 func main() {
 	// fmt.Println(calc.aaa) 这里肯定是报错的，不能是小写的
 	fmt.Println(calc.Add(16, 20))
+
+	a := decimal.NewFromFloat32(64.3)
+	b := decimal.NewFromInt(43)
+	fmt.Println(a.Add(b))
+
 }
