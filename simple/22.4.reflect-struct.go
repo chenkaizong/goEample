b977d8276481b9309411d147728c4f6c2a1938e6
ckz <ckz>
2023-03-05 23:05:08 +0800

    reflect

--- simple/22.4.reflect-struct.go
+++ simple/22.4.reflect-struct.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type Person struct {
 	Name  string
 	Age   int
@@ -7,9 +12,13 @@ type Person struct {
 }
 
 func PrintStructFields(s interface{}) {
-
+	t := reflect.TypeOf(s)
+	if t.Kind() != reflect.Struct && t.Elem().Kind() != reflect.Struct {
+		fmt.Println("不是结构题类")
+	}
 }
 
 func main() {
-
+	var person Person = Person{"name", 10, 11.2}
+	PrintStructFields(person)
 }
